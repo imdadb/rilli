c61e7d890ee81dc4da59ff9539828d5902495653
imdadb <imdadb@gmail.com>
2025-06-11 04:45:22 +0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
           <Route path="/*" element={
             <MainLayout>
               <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/\" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/dashboard" element={
                   <ProtectedRoute>
