436ff61f0a79c51aaef03f4b0ded68f34c1478a3
imdadb <imdadb@gmail.com>
2025-06-11 05:01:45 +0500

    Remove JSX icons from menu configuration

--- src/config/menuConfig.ts
+++ src/config/menuConfig.ts
@@ -1,10 +1,4 @@
 import React from 'react';
-import {
-  Dashboard as DashboardIcon,
-  People as PeopleIcon,
-  AccountBalance as FinanceIcon,
-  School as ClassesIcon,
-} from '@mui/icons-material';
 
 export interface MenuItem {
   label: string;
@@ -17,25 +11,21 @@ export const menuConfig: MenuItem[] = [
   { 
     label: 'Dashboard', 
     path: '/dashboard', 
-    icon: <DashboardIcon />, 
     permission: '*' 
   },
   { 
     label: 'Users', 
     path: '/users', 
-    icon: <PeopleIcon />, 
     permission: 'see_users' 
   },
   { 
     label: 'Finance', 
     path: '/finance', 
-    icon: <FinanceIcon />, 
     permission: 'see_finance' 
   },
   { 
     label: 'Classes', 
     path: '/classes', 
-    icon: <ClassesIcon />, 
     permission: 'see_classes' 
   }
 ];
